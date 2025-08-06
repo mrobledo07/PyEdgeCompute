38284ba38730b4847c18c41add80e8743fd6af6c
Miguel Robledo <125602276+mrobledo07@users.noreply.github.com>
2025-08-06 13:27:10 +0200

    ami id, orchestrator ip, terasort config

--- scripts/deploy_workers_aws.sh
+++ scripts/deploy_workers_aws.sh
@@ -11,7 +11,7 @@ WORKER_COUNT="$1"
 # (shall connect to the orchestrator at initialization)
 INSTANCE_ROLE="MyEC2S3AccessRole"
 KEY_PAIR_NAME="worker-keys" # Use your .pem key name
-AMI_ID="ami-094ea41c95c9a1c17" # Use the worker AMI ID
+AMI_ID="ami-062abdb4b1d1cc0bf" # Use the worker AMI ID
 INSTANCE_TYPE="t3.medium"
 
 aws ec2 run-instances \
